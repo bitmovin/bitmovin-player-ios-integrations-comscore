3bc068912323547c4f5451bd37fedfcc92a1c3be
Cory Zachman <cory.zachman@bitmovin.com>
2019-02-25 14:36:01 -0700

    updated tvOS viewcontroller to have autoplay turned on and a working playerView

--- Example/BitmovinComScoreAnalytics_Example_tvOS/ViewController.swift
+++ Example/BitmovinComScoreAnalytics_Example_tvOS/ViewController.swift
@@ -12,10 +12,6 @@ import BitmovinPlayer
 import ComScore
 
 class ViewController: UIViewController {
-    let adTagVastSkippable = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator="
-    let adTagVast1 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
-    let adTagVast2 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/32573358/2nd_test_ad_unit&ciu_szs=300x100&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator="
-    
     var bitmovinPlayer: BitmovinPlayer?
     @IBOutlet var playerView: UIView!
     var comScoreStreamingAnalytics: ComScoreStreamingAnalytics?
@@ -28,7 +24,6 @@ class ViewController: UIViewController {
     }
     
     func createPlayer() {
-        self.view.backgroundColor = .black
         
         // Define needed resources
         guard let streamUrl = URL(string: "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8") else {
@@ -37,6 +32,7 @@ class ViewController: UIViewController {
         
         // Create player configuration
         let config = PlayerConfiguration()
+        config.playbackConfiguration.isAutoplayEnabled = true
                 
         do {
             try config.setSourceItem(url: streamUrl)
@@ -45,6 +41,7 @@ class ViewController: UIViewController {
             let player = BitmovinPlayer(configuration: config)
             self.bitmovinPlayer = player
             
+            self.bitmovinPlayer?.add(listener: self)
             // Create a Comscore Configuration
             let comScoreMetadata: ComScoreMetadata = ComScoreMetadata(mediaType: .longFormOnDemand, publisherBrandName: "ABC", programTitle: "Modern Family", episodeTitle: "Rash Decisions", episodeSeasonNumber: "1", episodeNumber: "2", contentGenre: "Comedy", stationTitle: "Hulu", completeEpisode: true)
             
@@ -74,9 +71,14 @@ class ViewController: UIViewController {
             } else {
                 bitmovinPlayerView?.player = bitmovinPlayer
             }
-            
         } catch {
             print("Configuration error: \(error)")
         }
     }    
+}
+
+extension ViewController: PlayerListener {
+    func onError(_ event: ErrorEvent) {
+        print("On Error")
+    }
 }
