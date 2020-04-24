d33639ee9c740c2b50a966daceafe69a070eea12
aneurinc <nsc9012@gmail.com>
2020-04-24 11:14:47 -0600

    Fire one call for multiple labels

--- BitmovinComscoreAnalytics/Classes/ComScoreAnalytics.swift
+++ BitmovinComscoreAnalytics/Classes/ComScoreAnalytics.swift
@@ -5,11 +5,11 @@ public final class ComScoreAnalytics {
     private static let serialQueue = DispatchQueue(label: "com.bitmovin.player.integrations.comscore.ComScoreAnalytics")
     private static var started: Bool = false
     private static var configuration: ComScoreConfiguration?
-
+    
     /**
-     Starts the ComScoreAnalytics monitoring. Must be called before creating a ComScoreStreamingAnalytics object
+     Start ComScoreAnalytics app level tracking
      - Parameters:
-     - configuration: The ComScoreConfiguration that contains your application specific information
+     - configuration: The ComScoreConfiguration that contains application specific information
      */
     public static func start(configuration: ComScoreConfiguration) {
         serialQueue.sync {
@@ -30,10 +30,11 @@ public final class ComScoreAnalytics {
             }
         }
     }
-
+    
     /**
-     Sets the user consent to granted. Use after the ComScoreAnalytics object has been started
-    */
+     Set user consent to ComScoreUserConsent.GRANTED
+     */
+    @available(*, deprecated, message: "Deprecated as of release 1.3.0")
     public static func userConsentGranted() {
         serialQueue.sync {
             if started {
@@ -43,10 +44,11 @@ public final class ComScoreAnalytics {
             }
         }
     }
-
+    
     /**
-     Sets the user consent to denied. Use after the ComScoreAnalytics object has been started
-    */
+     Set user consent to ComScoreUserConsent.DENIED
+     */
+    @available(*, deprecated, message: "Deprecated as of release 1.3.0")
     public static func userConsentDenied() {
         serialQueue.sync {
             if started {
@@ -56,7 +58,37 @@ public final class ComScoreAnalytics {
             }
         }
     }
-
+    
+    /**
+     Set a persistent label on the ComScore PublisherConfiguration
+     - Parameters:
+     - label: The label to set
+     */
+    public static func setPersistentLabel(label: (String, String)) {
+        serialQueue.sync {
+            if started {
+                let publisherConfig = SCORAnalytics.configuration().publisherConfiguration(withPublisherId: ComScoreAnalytics.configuration?.publisherId)
+                publisherConfig?.setPersistentLabelWithName(label.0, value: label.1)
+                SCORAnalytics.notifyHiddenEvent()
+            }
+        }
+    }
+    
+    /**
+     Set persistent labels on the ComScore PublisherConfiguration
+     - Parameters:
+     - label: The labels to set
+     */
+    public static func setPersistentLabels(labels: [String: String]) {
+        serialQueue.sync {
+            if started {
+                let publisherConfig = SCORAnalytics.configuration().publisherConfiguration(withPublisherId: ComScoreAnalytics.configuration?.publisherId)
+                labels.forEach { publisherConfig?.setPersistentLabelWithName($0.0, value: $0.1) }
+                SCORAnalytics.notifyHiddenEvent(withLabels: labels)
+            }
+        }
+    }
+    
     public static func createComScoreStreamingAnalytics(bitmovinPlayer: BitmovinPlayer, metadata: ComScoreMetadata) throws -> ComScoreStreamingAnalytics? {
         if started {
             return ComScoreStreamingAnalytics(bitmovinPlayer: bitmovinPlayer, configuration: ComScoreAnalytics.configuration!, metadata: metadata)
@@ -64,7 +96,7 @@ public final class ComScoreAnalytics {
             throw ComScoreError.notStarted
         }
     }
-
+    
     public static func isActive() -> Bool {
         return ComScoreAnalytics.started
     }
--- BitmovinComscoreAnalytics/Classes/ComScoreStreamingAnalytics.swift
+++ BitmovinComscoreAnalytics/Classes/ComScoreStreamingAnalytics.swift
@@ -10,10 +10,10 @@ import BitmovinPlayer
 
 public class ComScoreStreamingAnalytics {
     let comScoreAdapter: ComScoreBitmovinAdapter
-
+    
     // MARK: - initializer
     /**
-     Initializes a new Bitmovin ComScore Streaming Analytics object
+     Initialize a ComScoreStreamingAnalytics
      
      - Parameters:
      - player: BitmovinPlayer instance to track
@@ -22,36 +22,56 @@ public class ComScoreStreamingAnalytics {
     init(bitmovinPlayer: BitmovinPlayer, configuration: ComScoreConfiguration, metadata: ComScoreMetadata) {
         self.comScoreAdapter = ComScoreBitmovinAdapter(player: bitmovinPlayer, configuration: configuration, metadata: metadata)
     }
-
+    
     deinit {
         self.comScoreAdapter.destroy()
     }
-
+    
     /**
-     Destroys the ComScoreStreaming analytics object and unregisters it from the player
-    */
+     Destroy ComScoreStreamingAnalytics and unregister it from player
+     */
     public func destroy() {
         self.comScoreAdapter.destroy()
     }
-
+    
     /**
-     Updates the metadata that is being tracked. Use this method when switching assets. Update metadata after unloading the old content and before loading the new content 
+     Update metadata for tracked source. This should be called when changing sources.
      */
     public func update(metadata: ComScoreMetadata) {
         comScoreAdapter.update(metadata: metadata)
     }
-
+    
     /**
-     Sets the user consent value to granted
-    */
+     Set user consent to ComScoreUserConsent.GRANTED
+     */
+    @available(*, deprecated, message: "Deprecated as of release 1.3.0")
     public func userConsentGranted() {
         comScoreAdapter.userConsentGranted()
     }
-
+    
     /**
-     Sets the user consent value to denied
-    */
+     Set user consent to ComScoreUserConsent.DENIED
+     */
+    @available(*, deprecated, message: "Deprecated as of release 1.3.0")
     public func userConsentDenied() {
         comScoreAdapter.userConsentDenied()
+    }
+    
+    /**
+     Set a persistent label on the ComScore PublisherConfiguration
+     - Parameters:
+     - label: The label to set
+     */
+    public func setPersistentLabel(label: (String, String)) {
+        comScoreAdapter.setPersistentLabel(label: label)
+    }
+    
+    /**
+     Set persistent labels on the ComScore PublisherConfiguration
+     - Parameters:
+     - label: The labels to set
+     */
+    public func setPersistentLabels(labels: [String: String]) {
+        comScoreAdapter.setPersistentLabels(labels: labels)
     }
 }
