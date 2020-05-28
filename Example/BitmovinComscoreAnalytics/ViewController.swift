4588afd3889b5fec0cb1782a5df4aa75cf056b51
aneurinc <nsc9012@gmail.com>
2020-05-28 11:23:28 -0600

    Update sample app

--- Example/BitmovinComscoreAnalytics/ViewController.swift
+++ Example/BitmovinComscoreAnalytics/ViewController.swift
@@ -82,7 +82,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func vodButtonClicked(sender: UIButton) {
-        guard let streamUrl = URL(string: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8") else {
+        guard let streamUrl = URL(string: "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8") else {
             return
         }
 
